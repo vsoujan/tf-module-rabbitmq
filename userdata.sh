20187508f2db827b8726651a0b76e2769ba3bab7
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-09-22 01:10:42 +0530

    ansible

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-yum install ansible -y &>>/opt/userdata.log
+yum install ansible python3.11-pip.noarch -y &>>/opt/userdata.log
+pip3.11 install botocore boto3 &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/vsoujan/roboshop-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
